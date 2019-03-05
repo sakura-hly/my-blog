f830de734592b6413a20aa176cd9f7392e7f9949
sakura-hly <1612584677@qq.com>
2019-03-05 10:42:54 +0800

    Create InterruptCheck.java

---
+++ com/moon/demo/InterruptCheck.java
@@ -0,0 +1,22 @@
+package com.moon.demo;
+
+public class InterruptCheck {
+    public static void main(String[] args) {
+        Thread t = Thread.currentThread();
+        System.out.println("Point A: t.isInterrupted()=" + t.isInterrupted());
+
+        //待决中断，中断自身
+        t.interrupt();
+        System.out.println("Point B: t.isInterrupted()=" + t.isInterrupted());
+        System.out.println("Point C: t.isInterrupted()=" + t.isInterrupted());
+
+        try {
+            Thread.sleep(2000);
+            System.out.println("was NOT interrupted");
+        } catch (InterruptedException e) {
+            System.out.println("was interrupted");
+        }
+        //抛出异常后，会清除中断标志，这里会返回false
+        System.out.println("Point D: t.isInterrupted()=" + t.isInterrupted());
+    }
+}
