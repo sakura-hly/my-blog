aea7536e8926e7ecad152167a24307425d965a35
sakura-hly <1612584677@qq.com>
2020-01-15 03:42:27 +0000

    fix quicksort

--- 算法相关/Sort/QuickSort.java
+++ 算法相关/Sort/QuickSort.java
@@ -2,30 +2,35 @@ import java.util.Arrays;
 
 public class QuickSort {
   public static void main(String[] args) {
-    int[] arr = {2019, 4, 1, 2, 40, 54, 117000};
-    quickSort(arr, 0, arr.length - 1);
-    System.out.println(Arrays.toString(arr));
+    int[] nums = {2019, 4, 1, 2, 40, 54, 117000};
+    quickSort(nums, 0, nums.length - 1);
+    System.out.println(Arrays.toString(nums));
   }
 
-  private static void quickSort(int arr[], int left, int right) {
+  private static void quickSort(int nums[], int left, int right) {
     if (left < right) {
-      int pivot = partition(arr, left, right);
-      quickSort(arr, left, pivot - 1);
-      quickSort(arr, pivot + 1, right);
+      int pivot = partition(nums, left, right);
+      quickSort(nums, left, pivot - 1);
+      quickSort(nums, pivot + 1, right);
     }
   }
 
-  private static int partition(int arr[], int left, int right) {
-    int pivot = arr[right];
+  private static int partition(int nums[], int left, int right) {
+    int pivot = nums[right], hi = right;
     while (left < right) {
-      while (left < right && arr[left] < pivot) left++;
-      while (left < right && arr[right] > pivot) right--;
-
-      int temp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = temp;
+      if (nums[left] >= pivot) {
+          swap(nums, left, --right);
+      } else {
+          left++;
+      }
     }
-    
-    return left;
+    swap(nums, right, hi);
+    return right;
+  }
+
+  private static void swap(int[] nums, int left, int right) {
+    int temp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = temp;
   }
 }
